8a49016ef70bac13a2f4c5b7d706dd18bb753d2d
lousl <dagger126@126.com>
2016-03-31 09:56:17 +0800

    fix open encoding

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = open(os.path.join(here, 'README.md'), encoding='utf-8').read()
 
 setup (
     name='mdfs',
