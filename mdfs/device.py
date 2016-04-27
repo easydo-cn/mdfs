de02a6d5cf902e1d99ca285228f7de5b2b23a756
panjunyong <panjunyong@everydo.com>
2016-04-27 16:53:55 +0800

    简化转换文件的存储

--- mdfs/device.py
+++ mdfs/device.py
@@ -77,7 +77,7 @@ class StorageDeviceManager:
 
     def get_cache_key(self, key, mime='', subpath=''):
         mime = mime.replace('/', '_')
-        return key + '/.frs.' + mime + '/' + subpath
+        return key + '.' + mime + '/' + subpath
 
     def os_path(self, name, key):
         device, cache_device = self.devices[name]
