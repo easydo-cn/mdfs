650d9483db1b02dfbf2978c7c4e37d4873c68e30
Joker Qyou <Joker.Qyou@gmail.com>
2016-06-01 15:37:45 +0800

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-################################
+# coding: utf-8
 import os
 from setuptools import setup, find_packages
 
@@ -13,7 +13,7 @@ setup(
     version='0.1',
     author = "panjy",
     author_email = "panjunyong@gmail.com",
-    description = "Site file storage",
+    description = "Multiple Device File Storage",
     long_description=README,
     license = "GPL",
     classifiers = [
@@ -23,13 +23,12 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
         ],
-    url = 'http://github.com/easydo-cn/sfs',
+    url = 'https://github.com/easydo-cn/mdfs',
     packages = find_packages(),
     include_package_data = True,
     install_requires = [
          'setuptools',
          'oss2',
-         # tests only
         ],
     zip_safe = False,
     entry_points = """
